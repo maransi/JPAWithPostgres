cc6df8e570a50629392bb70ccb4097d8ea1df47b
maransi <marco@paris>
2020-04-24 00:37:31 -0300

    Alteração feita no Eclipse

--- JPAWithPostgres/src/main/java/com/example/demo/model/Conta.java
+++ JPAWithPostgres/src/main/java/com/example/demo/model/Conta.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class Conta {
 // Alteração feita no Github
+// Alteração feita no Eclipse
+	
 	private Long id;
 	private String nomeCorrentista;
 	private Float saldo;
